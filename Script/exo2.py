0f1855c1457cb0c4878765d1c2e9b1ff1e283ead
nathan <nathan@dbNathan>
2023-04-19 22:50:00 +0200

    Allez Lucie

--- Script/exo2.py
+++ Script/exo2.py
@@ -2,7 +2,7 @@ from sklearn.decomposition import PCA
 import pandas as pd
 
 # Importer les données
-data = pd.read_csv("../Files/exo1File.csv")
+data = pd.read_csv("../Files/exoplanetsExo1.csv")
 
 # Appliquer une réduction de dimension avec PCA
 pca = PCA(n_components=2)
@@ -10,5 +10,5 @@ data_pca = pca.fit_transform(data)
 
 # Sauvegarder les données réduites dans un nouveau fichier CSV
 data_pca = pd.DataFrame(data_pca, columns=["PC1", "PC2"])
-data_pca.to_csv("../Files/exo2File.csv", index=False)
+data_pca.to_csv("../Files/exoplanetsExo2.csv", index=False)
 
