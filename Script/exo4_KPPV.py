a5405b9bed582ea249a264ef7a24b749a50194c4
Astrig Noemie <noemie.varjabedian@gmail.com>
2023-04-20 04:59:39 +0000

    exo4 full

--- Script/exo4_KPPV.py
+++ Script/exo4_KPPV.py
@@ -1,6 +1,7 @@
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import accuracy_score, precision_score, f1_score
 import pandas as pd
+import time
 
 # Charger les données
 data = pd.read_csv('exoplanetsExo2.csv')
@@ -16,11 +17,16 @@ y_test = dataTest['koi_disposition']
 # Initialisation du classificateur KNN avec un nombre de voisins égal à 5
 knn = KNeighborsClassifier(n_neighbors=5)
 
+
+start = time.time()
 # Entraînement du classificateur sur les données d'apprentissage
 knn.fit(X_train, y_train)
 
 # Prédiction des étiquettes pour les données de test
 y_pred = knn.predict(X_test)
+
+end = time.time()
+elapsed = end - start
 
 # Calcul de l'accuracy, précision et f1_score du modèle
 accuracy = accuracy_score(y_test, y_pred)
@@ -32,18 +38,22 @@ y_confirmed = [i for i in y_test if i == 1]
 y_candidate = [i for i in y_test if i == 2]
 y_falsepositive = [i for i in y_test if i == 0]
 
+# Temps d'éxecution
+print ("Temps d'éxecution : {:.2f}ms".format(elapsed))
+
+# Accuracy du modèle
 print("Exactitude (accuracy) du modèle : {:.2f}%".format(accuracy * 100))
 print()
 # Précision du modèle
-print("Precision pour confirmed:", precision[1])
-print("Precision pour candidate:", precision[2])
-print("Precision pour false positive:", precision[0])
+print("Precision pour confirmed: {:.2f}%".format(precision[1] * 100))
+print("Precision pour candidate: {:.2f}%".format(precision[2] * 100))
+print("Precision pour false positive: {:.2f}%".format(precision[0] * 100))
 
-print ("Precision du modèle:", (precision[1] * len(y_confirmed) + precision[2] * len(y_candidate) + precision[0] * len(y_falsepositive)) / len(y_test))
+print ("Precision du modèle: {:.2f}%".format((precision[1] * len(y_confirmed) + precision[2] * len(y_candidate) + precision[0] * len(y_falsepositive)) / len(y_test) * 100))
 print()
 # F1_score du modèle
-print("F1_score pour confirmed:", f1_score[1])
-print("F1_score pour candidate:", f1_score[2])
-print("F1_score pour false positive:", f1_score[0])
+print("F1_score pour confirmed: {:.2f}%".format(f1_score[1] * 100))
+print("F1_score pour candidate: {:.2f}%".format(f1_score[2] * 100))
+print("F1_score pour false positive: {:.2f}%".format(f1_score[0] * 100))
 
-print ("F1_score du modèle:", (f1_score[1] * len(y_confirmed) + f1_score[2] * len(y_candidate) + f1_score[0] * len(y_falsepositive)) / len(y_test))
+print ("F1_score du modèle: {:.2f}%".format((f1_score[1] * len(y_confirmed) + f1_score[2] * len(y_candidate) + f1_score[0] * len(y_falsepositive)) / len(y_test) * 100))
