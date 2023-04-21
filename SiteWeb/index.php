52187d417c47ee4d1b76c8aeea7ca10ed82b4fb2
Tristan Barlet <Tristan.BARLET@etu.uca.fr>
2023-04-21 13:12:45 -0400

    modif site

--- SiteWeb/index.php
+++ SiteWeb/index.php
@@ -180,10 +180,11 @@ if (isset($_POST['isSubmit']) && $_POST['isSubmit']==1) {
     </table>
     </blockquote>
 
-    <blockquote class="introEnt blockquote mb-0">
-    <footer class="blockquote-footer">Ceci est le résultat du travail d'équipe de notre groupe (Lucie Bedouret, Nathan Verdier, Tristan Barlet, Noémie Varjabedian) </footer>7
-    </blockquote>
   </div>
+  
+  <blockquote class="introEnt blockquote mb-0">
+    <footer class="blockquote-footer">Ceci est le résultat du travail d'équipe de notre groupe (Lucie Bedouret, Nathan Verdier, Tristan Barlet, Noémie Varjabedian) </footer>7
+  </blockquote>
 
 
 
