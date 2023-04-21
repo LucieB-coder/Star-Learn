a5406b90b8ec8e915cc93b59cd0bf3ed075fecdd
Tristan Barlet <Tristan.BARLET@etu.uca.fr>
2023-04-21 11:54:12 -0400

    correctif mineur commentaire

--- Script/exo3_KPPV.py
+++ Script/exo3_KPPV.py
@@ -17,7 +17,7 @@ y_train = data['koi_disposition']
 X_test = dataTest.drop('koi_disposition', axis=1)
 y_test = dataTest['koi_disposition']
 
-# Initialisation du classificateur KNN avec un nombre de voisins égal à 5
+# Initialisation du classificateur KNN avec un nombre de voisins égal à 3
 knn = KNeighborsClassifier(n_neighbors=3)
 
 # Entraînement du classificateur sur les données d'apprentissage
